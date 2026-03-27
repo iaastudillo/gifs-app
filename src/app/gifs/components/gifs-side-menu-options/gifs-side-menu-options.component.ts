04eb398de2fd45ccc5f7955dd71b059f0f308ddb
Ivo Andrés Astudillo <ivo_astudillo_1@outlook.com>
2026-03-26 21:00:07 -0500

    clase componentes

---
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptions {}
---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GifsSideMenuComponent } from '../../components/gifs-side-menu.component/gifs-side-menu.component';
+
+@Component({
+  selector: 'app-dashboard-page',
+  standalone: true,
+  imports: [RouterOutlet, GifsSideMenuComponent],
+  templateUrl: './dashboard-page.component.html',
+})
+export default class DashboardPageComponent {}
+
+/*  TAREA 
+CREAR DOS COMPONENTES PARA ENLAZAR DOS RUTAS MÁS 
+UNA QUE SE LLAMA TRENDING Y OTRA SEARCH
+*/
